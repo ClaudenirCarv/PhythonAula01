a33de971f19c1f9fe001a23d67024f639eb46079
ClaudenirCarv <claudenir123karanba@gmail.com>
2024-09-16 16:49:00 -0300

    aula 01

---
+++ AULA01/ativ1.py
@@ -0,0 +1,38 @@
+# # msg = 'Algoritmos em Python'
+# # # print(msg)
+
+# # nome = 'Paulo'
+# # print('Olá', nome)
+
+# # nome = 'Paulo'
+# # cor = 'Azul'
+# # print(nome, cor)
+
+# nome = input('Informe o nome: ')
+# ano_nascimento = int(input('Informe o ano de nascimento: '))
+# altura = float(input('Informe a altura: '))
+# idade = 2024 - ano_nascimento
+
+
+# print(f'Seu nome é {nome}, o seu ano de nascimento é {ano_nascimento} e sua altura é {altura}')
+# print(f'A sua idade é {idade}')
+# # print(type(ano_nascimento))
+
+
+# num = float(input('Número: '))
+
+# dobro = num * 2
+# triplo = num * 3
+# quadrado = num ** 2
+
+# print(f'O dobro é {dobro}, o trplo {triplo} e o quadrado {quadrado}')
+
+
+
+
+teste = float(input('Informe a nota do teste: '))
+prova = float(input('Informe a nota da prova: '))
+
+media = (teste + prova) / 2
+
+print(f'A média é {media}')
---
+++ AULA01/main.py
@@ -0,0 +1,30 @@
+# print('Olá Mundo!!!')
+
+# string - cadeia de caracteres
+# int - Números inteiros
+# float - Números reais
+# bool - True ou False
+# CTRL + S - SALVAR
+# CTRL + ALT + N - RODAR
+
+# +
+# -
+# *
+# /
+# % - Módulo (Resto da Divisão)
+# // - parte inteira da divisão
+
+# ano_nascim = 1981
+# ano_atual = 2024
+# idade = ano_atual - ano_nascim
+# print(idade)
+
+# n1 = 5
+# n2 = 2
+
+# resultado = n1 // n2
+
+# print(resultado)
+
+
+print('olá')
